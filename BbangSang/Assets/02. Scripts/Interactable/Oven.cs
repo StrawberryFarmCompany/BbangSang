4cc8f5ce6c33ad4760feb8160a0e1ba8868f8479
Lee Hye Rim <37866532+lhr4426@users.noreply.github.com>
2025-07-31 22:02:51 +0900

    [feat] interactable 세팅 완료

---
+++ BbangSang/Assets/02. Scripts/Interactable/Oven.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oven : BaseInteractable
+{
+    public override void Interact()
+    {
+        Debug.Log("ø¿∫Ï¿ª ≈µ¥œ¥Ÿ.");
+    }
+}
