67a56182908630abe740ba687b6ea84458ab349d
SJW1737 <sic470212@naver.com>
2025-08-01 21:36:07 +0900

    [feat] UI닫기 열기 기본 로직(수정 필요)

--- BbangSang/Assets/02. Scripts/Interactable/CraftingTable.cs
+++ BbangSang/Assets/02. Scripts/Interactable/CraftingTable.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CraftingTable : BaseInteractable
 {
     public GameObject craftingUI;
+    private bool isUIOpen = false;
 
     public override void Interact()
     {
         craftingUI.SetActive(true);
+        Debug.Log("제작테이블 오픈");
     }
 }
