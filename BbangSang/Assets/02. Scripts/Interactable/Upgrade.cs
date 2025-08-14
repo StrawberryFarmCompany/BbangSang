5262fc9b34c9c6877e703cb6a30335c3bca79e2b
Seungmin7979 <safetmdals79@gmail.com>
2025-08-14 23:00:37 +0900

    [Feat] 업그레이드UI 뼈대 작업

---
+++ BbangSang/Assets/02. Scripts/Interactable/Upgrade.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : BaseInteractable
+{
+    [SerializeField] private GameObject upgradeUI;
+    public override void Interact()
+    {
+        if (upgradeUI != null)
+        {
+            upgradeUI.SetActive(true);
+            Debug.Log("레시피 테이블 보기");
+        }
+    }
+}
