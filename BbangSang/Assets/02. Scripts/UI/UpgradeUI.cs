b594344d7063372babc85c6d29745712b907ad3b
Seungmin7979 <safetmdals79@gmail.com>
2025-08-16 23:10:36 +0900

    [Feat & Chore] UpgradeUI 스크립트 추가 및 UI 글 수정

--- BbangSang/Assets/02. Scripts/UI/UpgradeUI.cs
+++ BbangSang/Assets/02. Scripts/UI/UpgradeUI.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button exit;
+    [SerializeField] private Button upgrade1;
+    [SerializeField] private Button upgrade2;
+    [SerializeField] private Button upgrade3;
+
+
+    private void Start()
     {
-        
+        exit.onClick.AddListener(ExitButton);
+        upgrade1.onClick.AddListener(UpgradeButton);
+        upgrade2.onClick.AddListener(UpgradeButton);
+        upgrade3.onClick.AddListener(UpgradeButton);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void UpgradeButton()
     {
-        
+        Debug.Log("업그레이드");
+    }
+
+    public void ExitButton()
+    {
+        gameObject.SetActive(false);
     }
 }
