ac00c5e00b8034a3351fac8b05df08221c7f87e6
lhr4426 <lhr4426@naver.com>
2025-09-01 22:41:00 +0900

    [feat] 반죽 갯수 선택 UI 생성 및 코드 일부 연결

--- BbangSang/Assets/02. Scripts/UI/InteractUIs/SelectDoughUI.cs
+++ BbangSang/Assets/02. Scripts/UI/InteractUIs/SelectDoughUI.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SelectDoughUI : MonoBehaviour
@@ -9,7 +10,9 @@ public class SelectDoughUI : MonoBehaviour
 
     [SerializeField] public Transform doughParent;
     [SerializeField] public GameObject doughPrefab;
-
+    [SerializeField] public SelectDoughCountUI selectDoughCountUI;
+    [SerializeField] public TextMeshProUGUI selectedDoughCount; 
+    
     private List<(int recipeId, int count)> doughs = new();
     
     
@@ -24,6 +27,7 @@ public class SelectDoughUI : MonoBehaviour
         {
             Debug.LogError("doughPrefab에 dough 클래스가 없음");
         }
+        selectDoughCountUI.Init(oven, this);
         RefreshUI();
     }
 
@@ -41,7 +45,12 @@ public class SelectDoughUI : MonoBehaviour
             {
                 GameObject go =  Instantiate(doughPrefab, doughParent);
                 Dough d = go.GetComponent<Dough>();
-                d.Init(dough.recipeId, dough.count);
+                d.Init(dough.recipeId, dough.count,
+                    (recipeId, doughCount) =>
+                    {
+                        selectDoughCountUI.Setting(recipeId, dough.count);
+                        selectDoughCountUI.gameObject.SetActive(true);
+                    });
                 go.SetActive(true);
             }
         }
@@ -53,5 +62,10 @@ public class SelectDoughUI : MonoBehaviour
         {
             Destroy(doughParent.GetChild(i).gameObject);
         }
+    }
+
+    public void ChangeSelectedDoughCount()
+    {
+        selectedDoughCount.text = oven.InOvenCount.ToString();
     }
 }
