29e28b4b8cd3a6a9ea39c9f717c972bc3dc6179c
myeoungeun <melee0110@naver.com>
2025-09-03 00:03:21 +0900

    [feat] 매대

--- BbangSang/Assets/02. Scripts/UI/DisplayUIs/DisplayChooseUI.cs
+++ BbangSang/Assets/02. Scripts/UI/DisplayUIs/DisplayChooseUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,13 +7,56 @@ using UnityEngine.UI;
 
 public class DisplayChooseUI : MonoBehaviour
 {
-    public GameObject displayChooseUI;
     public Button exit;
-    public TextMeshProUGUI menuText;
+    
+    public Image icon;
+    public TextMeshProUGUI breadName;
+    public TextMeshProUGUI breadCount;
+    public TMP_InputField inputBreadCount;
+    
+    private DisplayStandUI DisplayStandUI;
+    
+    private int currentBreadId;
+    private int currentBreadCount;
+    private int selectBreadCount = 0;
+    
+    public void Init(DisplayStandUI displayStandUI)
+    {
+        this.DisplayStandUI = displayStandUI;
+    }
 
     private void Start()
     {
         exit.onClick.AddListener(ExitButton);
+    }
+    
+    public void Setting(int recipeId, int count) //가지고 있는 빵 정보 UI에 표시
+    {
+        icon.sprite = BreadManager.Instance.GetRecipe(recipeId).GetIcon();
+        breadName.text = BreadManager.Instance.GetRecipe(recipeId).Name;
+        breadCount.text = count.ToString();
+        currentBreadId = recipeId;
+        currentBreadCount = count;
+    }
+    
+    public void ValidateCountText(string text) //inputField에서 숫자 입력 시 호출
+    {
+        if (text == String.Empty || text.Length == 0) return;
+        if (int.TryParse(text, out int result)) //숫자인 경우
+        {
+            inputBreadCount.text = Mathf.Min(result, currentBreadCount).ToString();
+            selectBreadCount = int.Parse(inputBreadCount.text); //실제 선택 수량 저장
+        }
+        else //필드 초기화
+        {
+            inputBreadCount.text = String.Empty; 
+        }
+    }
+    
+    public void SelectDisplay() //진열하기 눌렀을 때
+    {
+        if(selectBreadCount == 0) return; //개수 0이면 무시
+        
     }
 
     public void ExitButton()
--- BbangSang/Assets/02. Scripts/UI/DisplayUIs/DisplayStandUI.cs
+++ BbangSang/Assets/02. Scripts/UI/DisplayUIs/DisplayStandUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,14 +9,61 @@ public class DisplayStandUI : MonoBehaviour
     public GameObject displayStandUI;
     public GameObject displayChoose;
     public Button exit;
-    public List<(int recipeId, int count)> Buffer = new();
-    public DisContent selectedContent; //default null. 선택한 칸 확인용
+    
+    public Transform breadParent;
+    public GameObject breadPrefab;
+    public GameObject displayChooseObj;
+    private DisplayChooseUI displayChooseUI;
+    public TextMeshProUGUI selectedBreadCount; 
+    
+    private List<(int recipeId, int count)> breads = new();
+    
+    void Awake()
+    {
+        displayChooseUI = displayChooseObj.GetComponent<DisplayChooseUI>();
+    }
 
     private void Start()
     {
         exit.onClick.AddListener(ExitButton);
+        if (!breadPrefab.TryGetComponent(out Dough dough))
+        {
+            Debug.LogError("doughPrefab에 dough 클래스가 없음");
+        }
+        RefreshUI();
     }
+    
+    public void RefreshUI()
+    {
+        breads.Clear();
+        BreadManager.Instance.GetAllDough(breads); // doughs 에 현재 가지고 있는 반죽 아이디 : 개수 들어옴
+        ClearDoughs(); // 일단 doughParent 안에 있는 애들 싹 다 지움
 
+        //빵이 있을 때
+        if (breads.Count > 0)
+        {
+            foreach(var dough in breads)
+            {
+                GameObject go =  Instantiate(breadPrefab, breadParent); //슬롯 생성
+                Dough d = go.GetComponent<Dough>();
+                d.Init(dough.recipeId, dough.count,
+                    (recipeId, doughCount) =>
+                    {
+                        displayChooseUI.Setting(recipeId, dough.count); //수량 세팅
+                        displayChooseUI.gameObject.SetActive(true); //수량 UI 활성화
+                    });
+                go.SetActive(true);
+            }
+        }
+    }
+    
+    void ClearDoughs()
+    {
+        for (int i = 0; i < breadParent.childCount; i++)
+        {
+            Destroy(breadParent.GetChild(i).gameObject);
+        }
+    }
     public void ExitButton()
     {
         gameObject.SetActive(false);
@@ -24,11 +72,5 @@ public class DisplayStandUI : MonoBehaviour
     public void DisplayChooseButton()
     {
         displayChoose.gameObject.SetActive(true);
-    }
-
-    public void DisplayBread() //빵 전시
-    {
-        BreadManager.Instance.GetAllDough(Buffer); //(함수 호출할 때마다)현재 가지고 있는 빵 정보 가져오기
-        //인벤 목록에 뜨도록 하기
     }
 }
